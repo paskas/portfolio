ce053d347b73dc9cd9fc5cb44e172c9143cad10a
paskas <pascal.kasbeitzer@gmail.com>
2025-10-08 14:31:09 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './core/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainLayout }
+];
---
+++ src/app/core/main-layout/main-layout.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss'
+})
+export class MainLayout {
+
+}
