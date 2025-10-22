b665e29264f74efff10eef4d5f764983caea33be
paskas <pascal.kasbeitzer@gmail.com>
2025-10-22 20:57:42 +0200

    style:- added skill assets - adjusted skill content paths and names - completely redesigned skill set

--- src/app/pages/portfolio/content/skills.content.ts
+++ src/app/pages/portfolio/content/skills.content.ts
@@ -1,15 +1,15 @@
 import type { Skill } from '../content/content.types';
 
 export const SKILLS_DEFAULT: Skill[] = [
-  { id: 'html', label: 'HTML', iconSrc: 'assets/icons/' },
-  { id: 'css', label: 'CSS', iconSrc: 'assets/icons/' },
-  { id: 'js', label: 'JavaScript', iconSrc: 'assets/icons/' },
-  { id: 'ts', label: 'TypeScript', iconSrc: 'assets/icons/' },
-  { id: 'angular', label: 'Angular', iconSrc: 'assets/icons/' },
-  { id: 'firebase', label: 'Firebase', iconSrc: 'assets/icons/' },
-  { id: 'material', label: 'Material', iconSrc: 'assets/icons/' },
-  { id: 'git', label: 'Git', iconSrc: 'assets/icons/' },
-  { id: 'rest', label: 'REST-API', iconSrc: 'assets/icons/' },
-  { id: 'scrum', label: 'Scrum', iconSrc: 'assets/icons/' },
-  { id: 'growth', label: 'Growth mindset', iconSrc: 'assets/icons/' },
+  { id: 'html', label: 'HTML', iconSrc: 'assets/icons/skills/HTML.svg' },
+  { id: 'css', label: 'CSS', iconSrc: 'assets/icons/skills/CSS.svg' },
+  { id: 'js', label: 'JavaScript', iconSrc: 'assets/icons/skills/JavaScript.svg' },
+  { id: 'ts', label: 'TypeScript', iconSrc: 'assets/icons/skills/TypeScript.svg' },
+  { id: 'angular', label: 'Angular', iconSrc: 'assets/icons/skills/Angular.svg' },
+  { id: 'firebase', label: 'Firebase', iconSrc: 'assets/icons/skills/Firebase.svg' },
+  { id: 'material', label: 'Material', iconSrc: 'assets/icons/skills/Material-Design.svg' },
+  { id: 'git', label: 'Git', iconSrc: 'assets/icons/skills/Git.svg' },
+  { id: 'rest', label: 'REST-API', iconSrc: 'assets/icons/skills/Rest-Api.svg' },
+  { id: 'scrum', label: 'Scrum', iconSrc: 'assets/icons/skills/Scrum.svg' },
+  { id: 'growth', label: 'Growth mindset', iconSrc: 'assets/icons/skills/GrowthMindset.svg' },
 ];
