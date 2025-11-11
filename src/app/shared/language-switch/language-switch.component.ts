d254d7db772b08967f876df02da8260afe1c53aa
paskas <pascal.kasbeitzer@gmail.com>
2025-11-11 15:30:55 +0100

    feat(language-switch): styling, checked-switch animation und hover-effekt für 'de'/'en' labels

--- src/app/shared/language-switch/language-switch.component.ts
+++ src/app/shared/language-switch/language-switch.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-language-switch',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './language-switch.component.html',
   styleUrl: './language-switch.component.scss'
 })
 export class LanguageSwitchComponent {
+  currentLang: 'en' | 'de' = 'en';
 
+  setLanguage(lang: 'en' | 'de') {
+    this.currentLang = lang;
+    console.log(
+      `Language switched to: ${lang === 'en' ? 'English' : 'German'}`
+    );
+  }
+
+  toggleLanguage() {
+    this.currentLang = this.currentLang === 'en' ? 'de' : 'en';
+  }
 }
