6122a80cb173be09e434209d0cf9f3599097e993
paskas <pascal.kasbeitzer@gmail.com>
2025-11-19 13:23:52 +0100

    feat(legal-notice): complete page with legal notices and optimized layout - new page with legal notices created and integrated into existing layout - structure with headings, lists, contact information, and image rights added - list formatting and new design for email links added - typography for headings, paragraphs, and lists adjusted - translation EN/DE - shortened and adjusted link scss

--- src/app/pages/legal-notice/legal-notice.page.ts
+++ src/app/pages/legal-notice/legal-notice.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MainLayout } from '../../layout/main-layout/main-layout';
+import { TranslateModule } from '@ngx-translate/core';
+import { SHARED_UI } from '../../shared';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [MainLayout],
+  imports: [...SHARED_UI, MainLayout, TranslateModule],
   templateUrl: './legal-notice.page.html',
   styleUrl: './legal-notice.page.scss'
 })
