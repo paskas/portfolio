b6765454d35160c84a4ff9d59168759d4909fa71
paskas <pascal.kasbeitzer@gmail.com>
2025-11-20 12:27:46 +0100

    feat(privacy-policy): added responsive typography and style, support for soft hyphenation, and improved line break behavior - added soft hyphenation for better line breaks on mobile devices - responsive clamp() - enabled safe line breaks with overflow-wrap: anywhere for long headings - cleaned up heading styles and ensured correct line height for better readability - updated de.json to include soft hyphenation in translation strings - legal notice also adapted for responsive design

--- src/app/pages/privacy-policy/privacy-policy.page.ts
+++ src/app/pages/privacy-policy/privacy-policy.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MainLayout } from "../../layout/main-layout/main-layout";
 import { TranslateModule } from '@ngx-translate/core';
+import { SHARED_UI } from '../../shared';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [MainLayout, TranslateModule],
+  imports: [...SHARED_UI, MainLayout, TranslateModule],
   templateUrl: './privacy-policy.page.html',
   styleUrl: './privacy-policy.page.scss',
 })
